eb7f7e815478405a04483177d0c6618731d8c1f4
stackedup23 <admin@stackingtoolsai.com>
2025-09-19 15:15:30 -0500

    Update GA ID and fix build issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* === Google Analytics ======================================= */}
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                `,
+              }}
+            />
+          </>
+        )}
+      </head>
+
       <body className="bg-black min-h-screen text-center font-sans text-lg">
 
         {/* === Logo Banner ============================================ */}
